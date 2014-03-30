5eedce5b6ec703c0fe81dad078b5c9fa17784d8f
mario <mario@mario-laptop-2.(none)>
2014-03-30 09:40:02 +0200

    minified the redirect example

--- r/index.php
+++ r/index.php
@@ -1,5 +1,4 @@
 <?php
-//longurl.org has length limits, hence directory name 'r' (not redirect)
-header('Location: http://html5sec.org/<script>alert(/a/)</script>/'); //I orginally used example.com (Didn't want to use example.invalid as some silly filters might block it)
+header('Location: https://html5sec.org/<script>alert(1)</script>/');
 die();
 ?>
